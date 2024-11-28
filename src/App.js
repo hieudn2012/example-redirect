89e23e878d64a7e7ad9225d5d8bfc61810cf3988
hieudn2012 <hieu.dn2012@gmail.com>
2024-11-28 12:06:48 +0700

    M

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const link = `https://google.com`;
+
+  const handleRedirect = () => {
+    window.location.href = link;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img
+        src='/close.png'
+        alt="close"
+        style={{
+          position: 'fixed',
+          top: '20px',
+          right: '20px',
+          width: '20px',
+          height: '20px',
+          cursor: 'pointer'
+        }}
+        onClick={handleRedirect}
+      />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          width: '100%',
+          backgroundColor: 'black',
+          color: 'white',
+          fontSize: '20px'
+        }}
+      >
+        <img
+          src='/threads.png'
+          alt='threads'
+          style={{
+            width: '100px',
+            height: '100px',
+            marginBottom: '20px'
+          }}
+        />
+        <p
+          style={{
+            fontSize: '30px',
+            maxWidth: '800px',
+            marginTop: '100px',
+          }}
+        >Warning: Content is for individuals aged 18 and over. Click "Continue" if you are 18 or older!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button
+          style={{ padding: '20px 40px', fontSize: '20px', cursor: 'pointer', borderRadius: '40px' }}
+          onClick={handleRedirect}
+        >Click here to continue</button>
+
+        <div style={{
+          marginTop: '100px',
+          color: 'gray',
+        }}>
+          <p>from</p>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '16px'
+            }}
+          >
+            <img
+              src='/meta.png'
+              alt='meta'
+              style={{
+                width: '40px',
+                height: '30px',
+              }}
+            />
+            <p
+              style={{
+                margin: 0,
+                fontSize: '30px',
+                fontWeight: 'medium'
+              }}
+            >Meta</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
